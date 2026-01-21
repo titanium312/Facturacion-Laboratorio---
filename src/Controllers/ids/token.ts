a0ab5948671f625b46a7eedce9c2ab72def75414
Roberto Barreto <rb.roberto.barreto@gmail.com>
2026-01-21 17:00:11 -0500

    inicial2

--- src/Controllers/ids/token.ts
+++ src/Controllers/ids/token.ts
@@ -2,5 +2,5 @@
 export const token = [
   { FacturadorId: 6874, nombre: "roberto", token: "AQSl6hWJhjPIqRE5FVxQEj2m+tBylqGIYr3XszOGeF8=.1SS9/UCeyjpq9PyT8MBqPg==.wcFkBNOeMUO3EbN8I4nUXw==" },
   { FacturadorId: 7033, nombre: "Carolina", token: '9EzGmwrc4l7mXxeGO45wQu0b32WPOMEksV+qa5shalY=.KdDZcpg075sszdBHAtSYRA==.wcFkBNOeMUO3EbN8I4nUXw==' },
-  { FacturadorId: 1, nombre: "", token: "" }
+  { FacturadorId: 6868, nombre: "Ana", token: '9EzGmwrc4l7mXxeGO45wQu0b32WPOMEksV+qa5shalY=.2rbpiJTaKTPia3JbwJZQEA==.wcFkBNOeMUO3EbN8I4nUXw==' }
 ];
