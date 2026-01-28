28f73ee671fb15d5ae4b0baaca386f286e0b4684
titanium312 <149283864+titanium312@users.noreply.github.com>
2026-01-28 01:34:52 -0500

    carilina id

--- src/Controllers/ids/token.ts
+++ src/Controllers/ids/token.ts
@@ -1,6 +1,6 @@
 // 🔹 Exportamos el array de tokens
 export const token = [
   { FacturadorId: 6874, nombre: "roberto", token:"AQSl6hWJhjPIqRE5FVxQEj2m+tBylqGIYr3XszOGeF8=.1SS9/UCeyjpq9PyT8MBqPg==.wcFkBNOeMUO3EbN8I4nUXw==" },
-  { FacturadorId: 7033, nombre: "Carolina", token:'a2gxVF2lkrT1W49nYnGLXWc33c+gIfKhEAf2M9k19+8=.KdDZcpg075sszdBHAtSYRA==.wcFkBNOeMUO3EbN8I4nUXw=='},
+  { FacturadorId: 7710, nombre: "Carolina", token:'a2gxVF2lkrT1W49nYnGLXWc33c+gIfKhEAf2M9k19+8=.KdDZcpg075sszdBHAtSYRA==.wcFkBNOeMUO3EbN8I4nUXw=='},
   { FacturadorId: 6868, nombre: "Ana", token:'9EzGmwrc4l7mXxeGO45wQu0b32WPOMEksV+qa5shalY=.2rbpiJTaKTPia3JbwJZQEA==.wcFkBNOeMUO3EbN8I4nUXw==' }
 ];
